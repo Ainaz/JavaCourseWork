21cdbc7d257364d67568ed13546e5f46aa1c9a6c
Ainaz <safin-ainaz@mail.ru>
2016-10-01 20:19:20 +0300

    Add a test class

--- Lists/src/ru/itis/IntegerArrayList.java
+++ Lists/src/ru/itis/IntegerArrayList.java
@@ -36,67 +36,58 @@ public class IntegerArrayList {
         else throw new IllegalArgumentException();
     }
 
-    /**
-     * Добавление элемента в заданную позицию - элементы
-     * правее позиции сдвигаются на один вправо
-     * @param element элемент
-     * @param position позиция, должна быть меньше, чем количество элементов
-     * @throws IllegalArgumentException если вставляем элемент в самый конец, а там
-     * элемент уже есть
-     */
     public void add(int element, int position){
         if(position < count && count < MAX_SIZE){
-            for (int i = this.data.length - 1; i > position; i--) {
-                this.data[i] = this.data[i - 1];
-            }
+            shiftingElementsInTheRight(position);
             this.data[position] = element;
-            count++;
         }
         else throw new IllegalArgumentException();
     }
 
-    /**
-     * Удаление элемента со сдвигом
-     * @param element элемент, который необходимо удалить
-     * @return true, если удаление прошло успешно, false - если элемент не был найден
-     */
     public boolean delete(int element){
         if (count >= 0 && find(element) != -1){
-            for (int i = this.data[find(element)] + 1; i < this.data.length; i++) {
-
-            }
-            //this.data[find(element)] =
+            shiftingElementsInTheLeft(find(element));
             return true;
         }
         else {
             return false;
-            }
+        }
     }
 
     public void deleteByPosition(int position){
-
+        if(position < count && count < MAX_SIZE){
+            shiftingElementsInTheLeft(position);
+        }
+        else throw new IllegalArgumentException();
     }
 
-    /**
-     * Поиск заданного элемента
-     * @param element искомый элемент
-     * @return индекс, в котором находится элемент. -1 - если элемент не найден
-     */
     public int find(int element){
-        int index = -1;
         for (int i = 0; i < this.data.length; i++) {
             if (this.data[i] == element) {
-                 index = this.data[i];
+                return i;
             }
         }
-        return index;
+        return -1;
     }
-
 
     public int get(int position){
         if(position < this.count){
             return this.data[position];
         }
         else throw new IllegalArgumentException();
+    }
+
+    private void shiftingElementsInTheRight(int position){
+        for (int i = this.count; i > position; i--) {
+            this.data[i] = this.data[i - 1];
+        }
+        this.count++;
+    }
+
+    private void shiftingElementsInTheLeft(int position){
+        for (int i = position; i < this.count; i++) {
+            this.data[i] = this.data[i + 1];
+        }
+        count--;
     }
 }
--- Lists/src/ru/itis/IntegerArrayListTest.java
+++ Lists/src/ru/itis/IntegerArrayListTest.java
@@ -21,7 +21,7 @@ public class IntegerArrayListTest {
     }
 
     @Test
-    public void testGet() throws Exception {
+    public void testGetOnCorrectData() throws Exception {
         int expected = 3;
 
         int actual = testedList.get(2);
@@ -29,5 +29,67 @@ public class IntegerArrayListTest {
         assertEquals(expected, actual);
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void testGetOnIncorrectData() throws Exception{
+        testedList.get(10);
+    }
+
+    @Test
+    public void testAddOnCorrectData() throws Exception{
+        int expected = 12;
+
+        testedList.add(12);
+
+        int actual = testedList.get(5);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testAddByPositionOnCorrectData() throws Exception{
+        int expected = 15;
+
+        testedList.add(15, 3);
+
+        int actual = testedList.get(3);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testAddByPositionOnIncorrectData() throws Exception{
+        testedList.add(20, 7);
+    }
+
+    @Test
+    public void testDeleteOnCorrectData() throws Exception{
+        int expected = 8;
+
+        testedList.delete(4);
+
+        int actual = testedList.get(3);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public  void testDeleteByPositionOnCorrectData() throws Exception{
+        int expected = 4;
+
+        testedList.deleteByPosition(2);
+
+        int actual = testedList.get(2);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testFindOnCorrectData() throws Exception{
+        int expected = 3;
+
+        int actual = testedList.find(4);
+
+        assertEquals(expected, actual);
+    }
 
 }
